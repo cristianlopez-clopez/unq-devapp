d120bced9159d9fd11c8bdd419940edc717226ff
cristianelopez <clopez@hexacta.com>
2016-04-19 18:26:39 -0300

    CAmbio de nombre del proyecto

--- src/wicket-spring-hib/src/main/java/ar/edu/unq/services/CarService.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.services;
-
-import ar.edu.unq.model.Car;
-
-public class CarService extends GenericService<Car> {
-
-	private static final long serialVersionUID = -2932116622242535843L;
-
-}
