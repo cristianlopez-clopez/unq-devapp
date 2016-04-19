13dcef28d9187d20bc272b503b1d1c2a79b15637
cristianelopez <clopez@hexacta.com>
2016-04-19 18:33:31 -0300

    Se borra wicket

--- src/spring-hibernate/target/classes/ar/edu/unq/view/components/ajaxLoadingButton/LoadingPanel.js
+++
@@ -1,9 +0,0 @@
-function showLoadingPanel(){
-	 $(".loadingPanel").addClass("loadingPanelActive");
-	 $(".loadingPanel").removeClass("loadingPanelInactive");
-}
-function hideLoadingPanel(){
-	$(".loadingPanel").addClass("loadingPanelInactive");
-	 $(".loadingPanel").removeClass("loadingPanelActive");
-}
-$(document).ready(hideLoadingPanel);
