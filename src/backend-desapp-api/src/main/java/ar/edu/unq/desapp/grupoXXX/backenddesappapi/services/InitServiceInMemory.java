2cae639ae68c3f41eeeae91a361665512366bb86
cristianelopez <cristianelopez@gmail.com>
2020-05-20 09:32:48 -0300

    Cabio para consumir la variable de entorno desde app.properties

--- src/backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoXXX/backenddesappapi/services/InitServiceInMemory.java
+++ src/backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoXXX/backenddesappapi/services/InitServiceInMemory.java
@@ -5,6 +5,7 @@ import javax.annotation.PostConstruct;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,13 +16,15 @@ import ar.edu.unq.desapp.grupoXXX.backenddesappapi.model.Car;
 public class InitServiceInMemory {
 
 	protected final Log logger = LogFactory.getLog(getClass());
+
+	@Value("${pring.datasource.driverClassName:NONE}")
+	private String className;
 	
 	@Autowired
 	private CarService carService;
 
 	@PostConstruct
 	public void initialize() {
-		String className = System.getenv("spring.datasource.driverClassName");
 		if (className.equals("org.h2.Driver")) {
 			logger.warn("Init Data Using H2 DB");
 			fireInitialData();
