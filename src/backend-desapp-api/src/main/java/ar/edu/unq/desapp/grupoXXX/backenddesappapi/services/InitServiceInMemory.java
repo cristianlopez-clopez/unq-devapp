a5e85d0a1c7dd7dde06ed17a3c62fbf4e44a7b70
cristianelopez <cristianelopez@gmail.com>
2020-05-20 08:44:15 -0300

    Se agrega ejemplo sobre como usar el logger

--- src/backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoXXX/backenddesappapi/services/InitServiceInMemory.java
+++ src/backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoXXX/backenddesappapi/services/InitServiceInMemory.java
@@ -2,6 +2,8 @@ package ar.edu.unq.desapp.grupoXXX.backenddesappapi.services;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +14,8 @@ import ar.edu.unq.desapp.grupoXXX.backenddesappapi.model.Car;
 @Transactional
 public class InitServiceInMemory {
 
+	protected final Log logger = LogFactory.getLog(getClass());
+	
 	@Autowired
 	private CarService carService;
 
@@ -19,6 +23,7 @@ public class InitServiceInMemory {
 	public void initialize() {
 		String className = System.getenv("spring.datasource.driverClassName");
 		if (className.equals("org.h2.Driver")) {
+			logger.warn("Init Data Using H2 DB");
 			fireInitialData();
 		}
 	}
